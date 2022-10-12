2297d7d423259d6ae6614a2d2481c4ef54571ec3
alamincoders <developeralamin03@gmail.com>
2022-10-13 02:20:50 +0600

    loader added

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,37 @@
-import Router from "next/router";
+import { ChakraProvider } from "@chakra-ui/react";
 import Head from "next/head";
+import Router from "next/router";
 import Nprogress from "nprogress";
 import Layout from "../components/Layout";
-import {ChakraProvider} from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
+  Nprogress.configure({ showSpinner: false });
+
+  Router.events.on("routeChangeStart", () => {
+    Nprogress.start();
+  });
+
+  Router.events.on("routeChangeComplete", () => {
+    Nprogress.done();
+  });
   return (
     <>
-    <Head>
-
-    </Head>
-    <ChakraProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
-
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+          integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </Head>
+      <ChakraProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
     </>
-  )
+  );
 }
 
 export default MyApp;
